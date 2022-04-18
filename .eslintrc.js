107603a91e95e55bf728fa581e2d60fa2d27c777
Thiago TM <70780578+xFsoH00@users.noreply.github.com>
2022-04-18 14:32:25 -0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+//npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
